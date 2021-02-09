f7a807195928300711a16295582f2592b10e64be
User <User@DESKTOP-0BJFLEG>
2021-02-09 11:29:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWThreadLocksAndMonitor
+{
+    class Program
+    {
+        public class Singleton
+        {
+            private static Singleton Instance;
+            private static object key = new object();
+            public static Singleton GetInstance()
+            {
+                if (Instance == null)
+                {
+                    lock (key)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new Singleton();
+                        }
+                    }
+                }
+                return Instance;
+            }
+
+            private Singleton()
+            {
+
+            }
+
+            public DateTime GetTime()
+            {
+                return DateTime.Now;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Singleton.GetInstance().GetTime());
+
+        }
+    }
+}
